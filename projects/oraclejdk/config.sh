562546e92bdeb2f285b566427bc708198e42c2b3
Ronny Trommer <ronny@opennms.org>
2019-02-18 20:31:04 +0100

    Update CentOS 7

--- projects/oraclejdk/config.sh
+++ projects/oraclejdk/config.sh
@@ -4,7 +4,7 @@
 
 # Base Image Dependency
 BASE_IMAGE="opennms/centos"
-BASE_IMAGE_VERSION="7.6.1810-b1"
+BASE_IMAGE_VERSION="7.6.1810-b2"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 
 # Version information
@@ -12,5 +12,5 @@ JDK_VERSION="8u201"
 JDK_BUILD_NUMBER="b09"
 JDK_RPM_URL="https://download.oracle.com/otn-pub/java/jdk/${JDK_VERSION}-${JDK_BUILD_NUMBER}/42970487e3af4f5aa5bca3f542482c60/jdk-${JDK_VERSION}-linux-x64.rpm"
 VERSION="${JDK_VERSION}-${JDK_BUILD_NUMBER}"
-BUILD_NUMBER="b1"
+BUILD_NUMBER="b2"
 IMAGE_VERSION="${VERSION}-${BUILD_NUMBER}"
--- projects/registry-config.sh
+++ projects/registry-config.sh
@@ -5,7 +5,7 @@
 # Container registry and tags
 CONTAINER_PROJECT="$(basename "$(pwd)")"
 CONTAINER_REGISTRY="docker.io"
-CONTAINER_REGISTRY_REPO="no42org"
+CONTAINER_REGISTRY_REPO="opennms"
 CONTAINER_VERSION_TAGS=("${IMAGE_VERSION}")
 
 # Container Image Artifact
