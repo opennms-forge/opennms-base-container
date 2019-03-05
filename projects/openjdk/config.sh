b03cb4bfbac33f3e7c94bf019aa026b049dd0bf8
Ronny Trommer <ronny@opennms.org>
2019-03-05 11:57:18 +0100

    Bump CentOS 7 dependency with updates and deltarpm

--- projects/openjdk/config.sh
+++ projects/openjdk/config.sh
@@ -4,11 +4,11 @@
 
 # Base Image Dependency
 BASE_IMAGE="opennms/centos"
-BASE_IMAGE_VERSION="7.6.1810-b2"
+BASE_IMAGE_VERSION="7.6.1810-b3"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 
 # Specific container config
 JDK_MAJOR_VERSION="1.8.0"
 JDK_VERSION_DETAIL="${JDK_MAJOR_VERSION}.191.b12"
-BUILD_NUMBER="b2"
+BUILD_NUMBER="b3"
 IMAGE_VERSION="${JDK_VERSION_DETAIL}-${BUILD_NUMBER}"
