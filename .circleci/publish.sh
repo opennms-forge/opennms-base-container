06e5264a159c36a0c0d8e10ed20cfce3f2014f47
Ronny Trommer <ronny@opennms.org>
2019-03-27 12:19:28 +0100

    Only publish images for master branch

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -7,6 +7,12 @@ source ./config.sh
 # shellcheck disable=SC1091
 source ../registry-config.sh
 
-for TAG in ${CONTAINER_VERSION_TAGS[*]}; do
-  docker push "${CONTAINER_REGISTRY}/${CONTAINER_REGISTRY_REPO}/${CONTAINER_PROJECT}:${TAG}"
-done
+if [ "${CIRCLE_BRANCH}" == "master" ]; then
+  echo "Publish images for master branch ..."
+  for TAG in ${CONTAINER_VERSION_TAGS[*]}; do
+    docker push "${CONTAINER_REGISTRY}/${CONTAINER_REGISTRY_REPO}/${CONTAINER_PROJECT}:${TAG}"
+  done
+else
+  echo "Skip publishing for working branches other than master."
+  echo "Build images for branches are available in the CircleCI build artifacts."
+fi
