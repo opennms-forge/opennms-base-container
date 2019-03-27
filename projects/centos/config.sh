e6d801fa5e9e0d5478399d86e8947eedbccf1d67
Ronny Trommer <ronny@opennms.org>
2019-03-27 09:11:01 +0100

    Add versions to CentOS repo

--- projects/centos/config.sh
+++ projects/centos/config.sh
@@ -5,6 +5,11 @@
 # Configure base image dependency
 BASE_IMAGE="centos"
 BASE_IMAGE_VERSION="7.6.1810"
+MAJOR_VERSION="7"
+FLOATING_VERSION="latest"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
-BUILD_NUMBER="b3"
-IMAGE_VERSION="${BASE_IMAGE_VERSION}-${BUILD_NUMBER}"
+BUILD_NUMBER="b4"
+IMAGE_VERSION="${BASE_IMAGE_VERSION}-${BUILD_NUMBER}
+               ${BASE_IMAGE_VERSION}
+               ${MAJOR_VERSION}
+               ${FLOATING_VERSION}"
