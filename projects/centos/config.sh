6a50ffabc62491bbd954140f3c1f2d663a5b26c1
Ronny Trommer <ronny@opennms.org>
2019-03-27 10:42:23 +0100

    Create array to allow different version tags and remove deltarpm from CentOS 7 which causes problems during updates

--- projects/centos/config.sh
+++ projects/centos/config.sh
@@ -5,11 +5,13 @@
 # Configure base image dependency
 BASE_IMAGE="centos"
 BASE_IMAGE_VERSION="7.6.1810"
+MINOR_VERSION="7.6"
 MAJOR_VERSION="7"
 FLOATING_VERSION="latest"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 BUILD_NUMBER="b4"
-IMAGE_VERSION="${BASE_IMAGE_VERSION}-${BUILD_NUMBER}
-               ${BASE_IMAGE_VERSION}
-               ${MAJOR_VERSION}
-               ${FLOATING_VERSION}"
+IMAGE_VERSION=("${BASE_IMAGE_VERSION}-${BUILD_NUMBER}"
+              "${BASE_IMAGE_VERSION}"
+              "${MINOR_VERSION}"
+              "${MAJOR_VERSION}"
+              "${FLOATING_VERSION}")
--- projects/registry-config.sh
+++ projects/registry-config.sh
@@ -6,7 +6,7 @@
 CONTAINER_PROJECT="$(basename "$(pwd)")"
 CONTAINER_REGISTRY="docker.io"
 CONTAINER_REGISTRY_REPO="opennms"
-CONTAINER_VERSION_TAGS=("${IMAGE_VERSION}")
+CONTAINER_VERSION_TAGS=${IMAGE_VERSION[*]}
 
 # Container Image Artifact
 CONTAINER_IMAGE="images/image.oci"
