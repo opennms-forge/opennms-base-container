d8e44672d3f3de16299dbbd26fb0b0cc7fa122bf
Ronny Trommer <ronny@opennms.org>
2019-04-03 21:28:47 +0200

    Add centos circleci build number to tag

--- projects/centos/config.sh
+++ projects/centos/config.sh
@@ -4,14 +4,20 @@
 
 # Configure base image dependency
 BASE_IMAGE="centos"
-BASE_IMAGE_VERSION="7.6.1810"
-MINOR_VERSION="7.6"
 MAJOR_VERSION="7"
+MINOR_VERSION="6"
+VERSION="${MAJOR_VERSION}.${MINOR_VERSION}.1810"
+BASE_IMAGE_VERSION="${VERSION}"
 FLOATING_VERSION="latest"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
-BUILD_NUMBER="b4"
-IMAGE_VERSION=("${BASE_IMAGE_VERSION}-${BUILD_NUMBER}"
-              "${BASE_IMAGE_VERSION}"
-              "${MINOR_VERSION}"
+BUILD_NUMBER="b5"
+IMAGE_VERSION=("${VERSION}-${BUILD_NUMBER}"
+              "${VERSION}"
               "${MAJOR_VERSION}"
+              "${MAJOR_VERSION}.${MINOR_VERSION}"
               "${FLOATING_VERSION}")
+
+# Most specific tag when it is not build locally and in CircleCI
+if [ -n "${CIRCLE_BUILD_NUM}" ]; then
+  IMAGE_VERSION+=("${VERSION}-${BUILD_NUMBER}.${CIRCLE_BUILD_NUM}")
+fi
