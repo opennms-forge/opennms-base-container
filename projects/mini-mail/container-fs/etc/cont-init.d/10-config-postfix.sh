269e3ff3e3be3d47bbe2783982c58ccd4b840d6e
Ronny Trommer <ronny@opennms.org>
2019-09-13 14:34:48 +0200

    Set postfix log to stdout

--- projects/mini-mail/container-fs/etc/cont-init.d/10-config-postfix.sh
+++ projects/mini-mail/container-fs/etc/cont-init.d/10-config-postfix.sh
@@ -10,3 +10,4 @@ postconf -e 'smtpd_sasl_auth_enable = yes'
 postconf -e 'smtpd_recipient_restrictions = permit_sasl_authenticated,permit_mynetworks'
 postconf -e 'smtpd_sasl_path = private/auth'
 postconf -e message_size_limit="${MESSAGE_SIZE_LIMIT}"
+postconf -e 'maillog_file = /dev/stdoud'
