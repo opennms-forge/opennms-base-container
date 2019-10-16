4ac78fff3b744d83f6cbec833c87b0aa70dc51d0
Ronny Trommer <ronny@opennms.org>
2019-10-16 17:29:12 +0200

    issue/4: Use CentOS 8 as base image
    Use CentOS major version as floating version and a fixed version composed with the CircleCI build number. Introduce
    opencontainers label specs and add opennms specific labels for base image and CI/CD information.

--- projects/centos/config.sh
+++ projects/centos/config.sh
@@ -4,20 +4,11 @@
 
 # Configure base image dependency
 BASE_IMAGE="centos"
-MAJOR_VERSION="7"
-MINOR_VERSION="6"
-VERSION="${MAJOR_VERSION}.${MINOR_VERSION}.1810"
-BASE_IMAGE_VERSION="${VERSION}"
-FLOATING_VERSION="latest"
+BASE_IMAGE_VERSION="8"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
-BUILD_NUMBER="b6"
-IMAGE_VERSION=("${VERSION}-${BUILD_NUMBER}"
-              "${VERSION}"
-              "${MAJOR_VERSION}"
-              "${MAJOR_VERSION}.${MINOR_VERSION}"
-              "${FLOATING_VERSION}")
+IMAGE_VERSION=("${BASE_IMAGE_VERSION}")
 
 # Most specific tag when it is not build locally and in CircleCI
 if [ -n "${CIRCLE_BUILD_NUM}" ]; then
-  IMAGE_VERSION+=("${VERSION}-${BUILD_NUMBER}.${CIRCLE_BUILD_NUM}")
+  IMAGE_VERSION+=("${BASE_IMAGE_VERSION}-b${CIRCLE_BUILD_NUM}")
 fi
