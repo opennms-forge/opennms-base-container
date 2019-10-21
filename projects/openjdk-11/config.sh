08fe615314d8510d3ee63172ab6581bda1633786
Ronny Trommer <ronny@opennms.org>
2019-10-21 08:44:40 +0200

    Build CentOS 7 OpenJDK versions

--- projects/openjdk-11/config.sh
+++ projects/openjdk-11/config.sh
@@ -7,11 +7,15 @@ CONTAINER_PROJECT="openjdk"
 
 # Base Image Dependency
 BASE_IMAGE="opennms/centos"
-BASE_IMAGE_VERSION="7.6.1810"
+BASE_IMAGE_VERSION="7-b2323"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 
 # Specific container config
 JDK_MAJOR_VERSION="11"
 JDK_VERSION_DETAIL="${JDK_MAJOR_VERSION}.0.4.11"
-IMAGE_VERSION=("${JDK_VERSION_DETAIL}"
-               "${JDK_MAJOR_VERSION}")
+IMAGE_VERSION=("${JDK_VERSION_DETAIL}")
+
+# Most specific tag when it is not build locally and in CircleCI
+if [ -n "${CIRCLE_BUILD_NUM}" ]; then
+  IMAGE_VERSION+=("${JDK_VERSION_DETAIL}-b${CIRCLE_BUILD_NUM}")
+fi
--- projects/openjdk/build_container_image.sh
+++ projects/openjdk/build_container_image.sh
@@ -11,11 +11,17 @@ source ../registry-config.sh
 docker build -t "${CONTAINER_PROJECT}:${IMAGE_VERSION[0]}" \
   --build-arg BASE_IMAGE="${BASE_IMAGE}" \
   --build-arg BASE_IMAGE_VERSION="${BASE_IMAGE_VERSION}" \
+  --build-arg BUILD_DATE="${BUILD_DATE}" \
+  --build-arg SOURCE="${CIRCLE_REPOSITORY_URL}" \
+  --build-arg REVISION="$(git describe --always)" \
+  --build-arg BUILD_JOB_ID="${CIRCLE_WORKFLOW_JOB_ID}" \
+  --build-arg BUILD_NUMBER="${CIRCLE_BUILD_NUM}" \
+  --build-arg BUILD_URL="${CIRCLE_BUILD_URL}" \
+  --build-arg BUILD_BRANCH="${CIRCLE_BRANCH}" \
   --build-arg JDK8_MAJOR_VERSION="${JDK8_MAJOR_VERSION}" \
   --build-arg JDK8_VERSION_DETAIL="${JDK8_VERSION_DETAIL}" \
   --build-arg JDK11_MAJOR_VERSION="${JDK11_MAJOR_VERSION}" \
   --build-arg JDK11_VERSION_DETAIL="${JDK11_VERSION_DETAIL}" \
-  --build-arg BUILD_DATE="${BUILD_DATE}" \
   .
 
 docker image save "${CONTAINER_PROJECT}:${IMAGE_VERSION[0]}" -o "${CONTAINER_IMAGE}"
