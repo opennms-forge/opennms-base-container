6274a8487922b46736129a5cc5a1e0c7e433a2c9
Ronny Trommer <ronny@opennms.org>
2019-10-21 15:51:30 +0200

    Bump to CentOS 8 for GoBGP and confd

--- projects/confd/config.sh
+++ projects/confd/config.sh
@@ -10,11 +10,9 @@ MAJOR_VERSION="0"
 VERSION="${MAJOR_VERSION}.16.0"
 CONFD_URL="https://github.com/kelseyhightower/confd/releases/download/v${VERSION}/confd-${VERSION}-linux-amd64"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
-BUILD_NUMBER="b1"
-IMAGE_VERSION=("${VERSION}-${BUILD_NUMBER}"
-              "${VERSION}")
+IMAGE_VERSION=("${VERSION}")
 
 # Most specific tag when it is not build locally and in CircleCI
 if [ -n "${CIRCLE_BUILD_NUM}" ]; then
-  IMAGE_VERSION+=("${VERSION}-${BUILD_NUMBER}.${CIRCLE_BUILD_NUM}")
+  IMAGE_VERSION+=("${VERSION}-b${CIRCLE_BUILD_NUM}")
 fi
