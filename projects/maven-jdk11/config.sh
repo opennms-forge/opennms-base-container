a7a88aa90e66199de7b114dcf0e3975e31cef348
Ronny Trommer <ronny@opennms.org>
2019-10-22 14:48:44 +0200

    Migrate Maven-jdk11 to use CentOS 8

--- projects/maven-jdk11/config.sh
+++ projects/maven-jdk11/config.sh
@@ -7,17 +7,20 @@ CONTAINER_PROJECT="maven"
 
 # Base Image Dependency
 BASE_IMAGE="opennms/openjdk"
-BASE_IMAGE_VERSION="11.0.3.7-b1"
+JDK_VERSION="11.0.4.11"
+MAVEN_VERSION="3.6.1"
+BASE_IMAGE_VERSION="${JDK_VERSION}-b2553"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 
 # Version information
-JDK_VERSION="jdk11"
-VERSION="3.6.1"
-BUILD_NUMBER="b1"
-IMAGE_VERSION=("${JDK_VERSION}_${VERSION}-${BUILD_NUMBER}"
-               "${JDK_VERSION}_${VERSION}")
+VERSION="jdk11-${MAVEN_VERSION}"
+IMAGE_VERSION=("${VERSION}")
 
-MAVEN_URL="https://www-eu.apache.org"
-MAVEN_PKG="${MAVEN_URL}/dist/maven/maven-3/${VERSION}/binaries/apache-maven-${VERSION}-bin.tar.gz"
-MAVEN_HOME="/opt/apache-maven-${VERSION}"
+MAVEN_HOST="https://www-eu.apache.org"
+MAVEN_URL="${MAVEN_HOST}/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz"
+MAVEN_HOME="/opt/apache-maven-${MAVEN_VERSION}"
 
+# Most specific tag when it is not build locally and in CircleCI
+if [ -n "${CIRCLE_BUILD_NUM}" ]; then
+  IMAGE_VERSION+=("${VERSION}-b${CIRCLE_BUILD_NUM}")
+fi
