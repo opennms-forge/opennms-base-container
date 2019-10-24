78c03e6b42c62444d7952cf3a2bd9fcdef5abe5e
Ronny Trommer <ronny@opennms.org>
2019-10-24 21:38:10 +0200

    Update Build Environment to CentOS 8

--- projects/build-env-jdk8/build_container_image.sh
+++ projects/build-env-jdk8/build_container_image.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-# shellcheck source=projects/build-env-jdk8/config.sh
+# shellcheck source=projects/build-env-jdk11/config.sh
 source ./config.sh
 
 # shellcheck source=projects/registry-config.sh
@@ -11,11 +11,17 @@ source ../registry-config.sh
 docker build -t "${CONTAINER_PROJECT}:${IMAGE_VERSION[0]}" \
   --build-arg BASE_IMAGE="${BASE_IMAGE}" \
   --build-arg BASE_IMAGE_VERSION="${BASE_IMAGE_VERSION}" \
-  --build-arg SHELLCHECK_VERSION="${SHELLCHECK_VERSION}" \
+  --build-arg BUILD_DATE="${BUILD_DATE}" \
   --build-arg VERSION="${VERSION}" \
-  --build-arg BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")" \
+  --build-arg SOURCE="${CIRCLE_REPOSITORY_URL}" \
+  --build-arg REVISION="$(git describe --always)" \
+  --build-arg BUILD_JOB_ID="${CIRCLE_WORKFLOW_JOB_ID}" \
+  --build-arg BUILD_NUMBER="${CIRCLE_BUILD_NUM}" \
+  --build-arg BUILD_URL="${CIRCLE_BUILD_URL}" \
+  --build-arg BUILD_BRANCH="${CIRCLE_BRANCH}" \
+  --build-arg SHELLCHECK_VERSION="${SHELLCHECK_VERSION}" \
   --build-arg PACKAGES="${PACKAGES}" \
-  --build-arg REPO_RELEASE="develop" \
+  --build-arg REPO_RELEASE="bleeding" \
   --build-arg REPO_HOST="${REPO_HOST}" \
   --build-arg REPO_KEY_URL="${REPO_KEY_URL}" \
   --build-arg ADD_RPMS="${ADD_RPMS}" \
