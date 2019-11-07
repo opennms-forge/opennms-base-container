8cd477f509a45d49209adf66974d180602d065a8
Ronny Trommer <ronny@opennms.org>
2019-11-07 09:35:55 +0100

    Update JDK 11.0.5.10

--- projects/openjdk-11/config.sh
+++ projects/openjdk-11/config.sh
@@ -12,7 +12,7 @@ BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 
 # Specific container config
 JDK_MAJOR_VERSION="11"
-JDK_VERSION_DETAIL="${JDK_MAJOR_VERSION}.0.4.11"
+JDK_VERSION_DETAIL="${JDK_MAJOR_VERSION}.0.5.10"
 IMAGE_VERSION=("${JDK_VERSION_DETAIL}")
 
 # Most specific tag when it is not build locally and in CircleCI
