b62a3a653d2cc25faa2891c051132f27cb16b3df
Benjamin Reed <ranger@opennms.org>
2019-11-13 11:45:41 -0500

    debian build environment images

---
+++ projects/debian-build-env-jdk/build_container_image.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+set -x
+
+# shellcheck source=projects/debian-build-env-jdk/config.sh
+source ./config.sh
+
+# shellcheck source=projects/registry-config.sh
+source ../registry-config.sh
+
+docker build -t "${CONTAINER_PROJECT}:${IMAGE_VERSION[0]}" \
+  --build-arg BASE_IMAGE="${BASE_IMAGE}" \
+  --build-arg BASE_IMAGE_VERSION="${BASE_IMAGE_VERSION}" \
+  --build-arg BUILD_DATE="${BUILD_DATE}" \
+  --build-arg VERSION="${VERSION}" \
+  --build-arg SOURCE="${CIRCLE_REPOSITORY_URL}" \
+  --build-arg REVISION="$(git describe --always)" \
+  --build-arg BUILD_JOB_ID="${CIRCLE_WORKFLOW_JOB_ID}" \
+  --build-arg BUILD_NUMBER="${CIRCLE_BUILD_NUM}" \
+  --build-arg BUILD_URL="${CIRCLE_BUILD_URL}" \
+  --build-arg BUILD_BRANCH="${CIRCLE_BRANCH}" \
+  --build-arg SHELLCHECK_VERSION="${SHELLCHECK_VERSION}" \
+  --build-arg PACKAGES="${PACKAGES}" \
+  --build-arg REPO_RELEASE="bleeding" \
+  --build-arg REPO_HOST="${REPO_HOST}" \
+  --build-arg REPO_KEY_URL="${REPO_KEY_URL}" \
+  --build-arg VM_TYPE="${VM_TYPE}" \
+  .
+
+docker image save "${CONTAINER_PROJECT}:${IMAGE_VERSION[0]}" -o "${CONTAINER_IMAGE}"
---
+++ projects/debian-build-env-jdk8/config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+# shellcheck disable=SC2034
+
+# Overwrite project name on DockerHub
+CONTAINER_PROJECT="build-env"
+
+# Base Image Dependency
+BASE_IMAGE="opennms/openjdk"
+BASE_IMAGE_VERSION="debian-jdk8-openj9-b3479"
+BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
+SHELLCHECK_VERSION="0.6.0"
+
+# Version information
+VERSION="debian-jdk8"
+IMAGE_VERSION=("${VERSION}")
+
+# Most specific tag when it is not build locally and in CircleCI
+if [ -n "${CIRCLE_BUILD_NUM}" ]; then
+  IMAGE_VERSION+=("${VERSION}-b${CIRCLE_BUILD_NUM}")
+fi
+
+REPO_RELEASE="stable"
+REPO_HOST="debian.opennms.org"
+REPO_KEY_URL="https://${REPO_HOST}/OPENNMS-GPG-KEY"
+
+PACKAGES="autoconf automake build-essential cmake curl debhelper devscripts dh-systemd docker-ce dpkg-sig expect git librrd-dev libtool make nsis po-debconf python python3 rsync shellcheck ssh thin-provisioning-tools"
