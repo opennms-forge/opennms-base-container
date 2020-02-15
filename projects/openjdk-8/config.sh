8b0784ef64efeb3564d499a1c11b21a774bd00d9
Ronny Trommer <ronny@opennms.org>
2020-02-15 20:43:12 +0100

    Update Java 8.242.b08

--- projects/openjdk-8/config.sh
+++ projects/openjdk-8/config.sh
@@ -12,7 +12,7 @@ BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 
 # Specific container config
 JDK_MAJOR_VERSION="1.8.0"
-JDK_VERSION_DETAIL="${JDK_MAJOR_VERSION}.232.b09"
+JDK_VERSION_DETAIL="${JDK_MAJOR_VERSION}.242.b08"
 IMAGE_VERSION=("${JDK_VERSION_DETAIL}")
 
 # Most specific tag when it is not build locally and in CircleCI
