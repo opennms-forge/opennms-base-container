c651b503bca3d2426f633c3705a89fb6bf13d65d
Ronny Trommer <ronny@opennms.org>
2020-02-26 23:30:09 +0100

    Add Ubuntu Eoan image with x86_64, ARMv7 and ARM64 support

---
+++ projects/ubuntu/config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+# shellcheck disable=SC2034
+
+# Configure base image dependency
+BASE_IMAGE="ubuntu"
+VERSION="eoan-20200207"
+BASE_IMAGE_VERSION="${VERSION}"
+BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
+IMAGE_VERSION=("${VERSION}")
+
+# Most specific tag when it is not build locally and in CircleCI
+if [ -n "${CIRCLE_BUILD_NUM}" ]; then
+  IMAGE_VERSION+=("${VERSION}-b${CIRCLE_BUILD_NUM}")
+fi
+
