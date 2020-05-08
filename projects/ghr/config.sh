f2ebaf0e76204dbf1e388d17d16436af1bcb79c3
Ronny Trommer <ronny@opennms.org>
2020-05-08 09:33:14 +0200

    Bump GitHub Release tool from 0.12.0 to 0.13.0

--- projects/ghr/config.sh
+++ projects/ghr/config.sh
@@ -8,9 +8,14 @@ BASE_IMAGE_VERSION="3.9"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 
 # Version information
-VERSION="0.12.0"
+VERSION="0.13.0"
 BUILD_NUMBER="b1"
 
 GHR_RELEASE_URL="https://github.com/tcnksm/ghr/releases/download/v${VERSION}/ghr_v${VERSION}_linux_amd64.tar.gz"
 
-IMAGE_VERSION="${VERSION}-${BUILD_NUMBER}"
+IMAGE_VERSION="${VERSION}"
+
+# Most specific tag when it is not build locally and in CircleCI
+if [ -n "${CIRCLE_BUILD_NUM}" ]; then
+  IMAGE_VERSION+=("${IMAGE_VERSION}-b${CIRCLE_BUILD_NUM}")
+fi
