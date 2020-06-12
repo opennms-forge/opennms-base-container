8dbaad162f8e7239ee89520b2956bb2747a01319
Ronny Trommer <ronny@opennms.org>
2020-06-12 12:39:11 +0200

    Add Minion running as root for GNS3
    Overwrite USER to run as root which is required for GNS3.

---
+++ projects/gns3-minion/config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+# shellcheck disable=SC2034
+
+# Overwrite project name on DockerHub
+CONTAINER_PROJECT="minion"
+
+# Base Image Dependency
+BASE_IMAGE="opennms/minion"
+BASE_IMAGE_VERSION="26.1.1"
+
+VERSION="${BASE_IMAGE_VERSION}-gns3"
+IMAGE_VERSION="${VERSION}"
+
+# Most specific tag when it is not build locally and in CircleCI
+if [ -n "${CIRCLE_BUILD_NUM}" ]; then
+  IMAGE_VERSION+=("${VERSION}-b${CIRCLE_BUILD_NUM}")
+fi
