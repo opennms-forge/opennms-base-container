1dd05857fa787a21352362090834ff60fc757314
Benjamin Reed <ranger@opennms.org>
2020-06-18 14:16:00 -0400

    always build everything if circle or common scripts have changed

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -2,11 +2,26 @@
 
 PROJECT="$(basename "$(pwd)")"
 
+found_changes() {
+  if git show --name-only --format='' | grep "^projects/registry-config.sh"; then
+    echo "registry-config.sh changed, triggering build"
+    return 0
+  fi
+  if git show --name-only --format='' | grep "^.circleci/"; then
+    echo "CircleCI config changed, triggering build"
+    return 0
+  fi
+  if git show --name-only --format='' | grep "^projects/${PROJECT}"; then
+    echo "${PROJECT} changed, triggering build"
+    return 0
+  fi
+  return 1
+}
+
 echo "Detect changes in projects"
-if git diff --name-only HEAD^...HEAD | grep "^projects/${PROJECT}"; then
-  echo "Changes here, run the build"
-  ./build_container_image.sh
-  ~/opennms-container/.circleci/tag.sh
+if found_changes; then
+  ./build_container_image.sh && \
+  ~/opennms-container/.circleci/tag.sh && \
   ~/opennms-container/.circleci/publish.sh
 else
   echo "No changes detected"
