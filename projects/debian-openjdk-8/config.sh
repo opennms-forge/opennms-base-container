971564cbddf9d91fec2570c28a9d10a268328906
Benjamin Reed <ranger@opennms.org>
2020-06-18 10:31:36 -0400

    switch openjdk packages to use debian builds rather than adoptopenjdk

--- projects/debian-openjdk-8/config.sh
+++ projects/debian-openjdk-8/config.sh
@@ -7,15 +7,17 @@ CONTAINER_PROJECT="openjdk"
 
 # Base Image Dependency
 BASE_IMAGE="opennms/debian"
-BASE_IMAGE_VERSION="stretch-b4175"
+BASE_IMAGE_VERSION="stretch-b4955"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 SHORT_DATE="$(date -u +"%Y%m%d%H%M%S")"
 
 VM_TYPE="openj9"
 
-IMAGE_VERSION=("debian-jdk8-${VM_TYPE}")
+#IMAGE_VERSION=("debian-jdk8-${VM_TYPE}")
+IMAGE_VERSION=("debian-jdk8")
 
 # Most specific tag when it is not build locally and in CircleCI
 if [ -n "${CIRCLE_BUILD_NUM}" ]; then
-  IMAGE_VERSION+=("debian-jdk8-${VM_TYPE}-b${CIRCLE_BUILD_NUM}")
+  #IMAGE_VERSION+=("debian-jdk8-${VM_TYPE}-b${CIRCLE_BUILD_NUM}")
+  IMAGE_VERSION+=("debian-jdk8-b${CIRCLE_BUILD_NUM}")
 fi
