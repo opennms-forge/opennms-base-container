134f5defa8205bfcf7c0bd90cb6a78734ab10f8e
Benjamin Reed <ranger@opennms.org>
2020-06-18 13:46:41 -0400

    attempt to make circle builds depend on each other

--- projects/registry-config.sh
+++ projects/registry-config.sh
@@ -14,3 +14,23 @@ CONTAINER_VERSION_TAGS=${IMAGE_VERSION[*]}
 
 # Container Image Artifact
 CONTAINER_IMAGE="images/image.oci"
+
+# if we have more than one $IMAGE_VERSION, that means
+# we got a specific build from CIRCLECI; save our
+# version to the `.circleci/build-numbers` directory
+if [ "${#IMAGE_VERSION[@]}" -gt 1 ]; then
+  _image_version="${IMAGE_VERSION[${#IMAGE_VERSION[@]}-1]}"
+  echo "project has a specific build image set, saving to build-numbers/${CONTAINER_PROJECT}: ${_image_version}"
+  echo "BASE_IMAGE_VERSION=${_image_version}" > "../../.circleci/build-numbers/${CONTAINER_PROJECT}"
+fi
+
+# if the parent build left a file in the `.circleci/build-numbers`
+# directory, source it to override the current $BASE_IMAGE_VERSION
+PARENT_PROJECT="${BASE_IMAGE/opennms\//}"
+if [ -e "../../.circleci/build-numbers/${PARENT_PROJECT}" ]; then
+  echo "found a parent project for this build: ${PARENT_PROJECT}"
+  echo "BASE_IMAGE_VERSION (before): $BASE_IMAGE_VERSION"
+  # shellcheck disable=SC1090
+  source "../../.circleci/build-numbers/${BASE_IMAGE/opennms\//}"
+  echo "BASE_IMAGE_VERSION (after): $BASE_IMAGE_VERSION"
+fi
