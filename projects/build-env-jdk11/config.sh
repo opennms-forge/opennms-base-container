898b4e959263094408887bc6c4cb514a863a5fbd
Benjamin Reed <ranger@opennms.org>
2020-09-24 14:38:39 -0400

    edit the right place >:S

--- projects/build-env-jdk11/config.sh
+++ projects/build-env-jdk11/config.sh
@@ -34,4 +34,32 @@ REPO_KEY_URL="https://${REPO_HOST}/OPENNMS-GPG-KEY"
 ADD_RPMS="https://${REPO_HOST}/repofiles/opennms-repo-${REPO_RELEASE}-rhel8.noarch.rpm \
           https://${REPO_HOST}/${REPO_RELEASE}/rhel7/nsis/mingw32-nsis-2.50-1.el7.centos.x86_64.rpm"
 
-PACKAGES="nodejs python2 python3 libxslt rsync gettext tree wget git-core openssh-clients which expect make cmake gcc-c++ jq rrdtool-devel automake libtool rpm-build redhat-rpm-config R-core jicmp jicmp6 jrrd2"
+PACKAGES="\
+  automake \
+  cmake \
+  expect \
+  gcc-c++ \
+  gettext \
+  git-core \
+  gnupg2 \
+  jicmp \
+  jicmp6 \
+  jrrd2 \
+  jq \
+  libtool \
+  libxslt \
+  make \
+  nodejs \
+  openssh-clients \
+  python2 \
+  python3 \
+  R-core \
+  redhat-rpm-config \
+  rpm-build \
+  rpm-sign \
+  rrdtool-devel \
+  rsync \
+  tree \
+  wget \
+  which \
+"
