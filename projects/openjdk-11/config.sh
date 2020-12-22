24b55d5138a5d777aacf7b91274875771cb63d1e
Ronny Trommer <ronny@opennms.org>
2020-12-22 20:12:38 +0100

    Update OpenJDK 11.0.7 to 11.0.9

--- projects/openjdk-11/config.sh
+++ projects/openjdk-11/config.sh
@@ -7,7 +7,7 @@ CONTAINER_PROJECT="openjdk"
 
 # Base Image Dependency
 BASE_IMAGE="opennms/centos"
-BASE_IMAGE_VERSION="8-b4056"
+BASE_IMAGE_VERSION="8-b5649"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 
 # inherit $BASE_IMAGE_VERSION
@@ -15,7 +15,7 @@ PARENT_PROJECT="centos"
 
 # Specific container config
 JDK_MAJOR_VERSION="11"
-JDK_VERSION_DETAIL="${JDK_MAJOR_VERSION}.0.7.10"
+JDK_VERSION_DETAIL="${JDK_MAJOR_VERSION}.0.9.11"
 IMAGE_VERSION=("${JDK_VERSION_DETAIL}")
 
 # Most specific tag when it is not build locally and in CircleCI
