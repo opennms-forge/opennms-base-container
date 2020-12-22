7e48bd1bee62288ec2691b58b3c3306658324229
Ronny Trommer <ronny@opennms.org>
2020-12-22 20:13:33 +0100

    Update OpenJDK 1.8.0.252.b09 to 1.8.0.272.b10

--- projects/openjdk-8/config.sh
+++ projects/openjdk-8/config.sh
@@ -7,7 +7,7 @@ CONTAINER_PROJECT="openjdk"
 
 # Base Image Dependency
 BASE_IMAGE="opennms/centos"
-BASE_IMAGE_VERSION="8-b4056"
+BASE_IMAGE_VERSION="8-b5649"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 
 # inherit $BASE_IMAGE_VERSION
@@ -15,7 +15,7 @@ PARENT_PROJECT="centos"
 
 # Specific container config
 JDK_MAJOR_VERSION="1.8.0"
-JDK_VERSION_DETAIL="${JDK_MAJOR_VERSION}.252.b09"
+JDK_VERSION_DETAIL="${JDK_MAJOR_VERSION}.272.b10"
 IMAGE_VERSION=("${JDK_VERSION_DETAIL}")
 
 # Most specific tag when it is not build locally and in CircleCI
