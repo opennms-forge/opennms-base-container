21808d75b7a60ed7c4c477d465217667e75a1084
Ronny Trommer <ronny@opennms.org>
2020-12-22 20:34:48 +0100

    Update Maven OpenJDK 11.0.9.11 and build-env with OpenJDK 1.8.0.272.b10

--- projects/maven-jdk11/config.sh
+++ projects/maven-jdk11/config.sh
@@ -7,9 +7,9 @@ CONTAINER_PROJECT="maven"
 
 # Base Image Dependency
 BASE_IMAGE="opennms/openjdk"
-JDK_VERSION="11.0.7.10"
+JDK_VERSION="11.0.9.11"
 MAVEN_VERSION="3.6.3"
-BASE_IMAGE_VERSION="${JDK_VERSION}-b4096"
+BASE_IMAGE_VERSION="${JDK_VERSION}-b5706"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 
 # inherit $BASE_IMAGE_VERSION
