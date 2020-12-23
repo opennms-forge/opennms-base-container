96e2f23e18a7b3f9067efbca2121ad5a7474163f
Ronny Trommer <ronny@opennms.org>
2020-12-23 08:06:50 +0100

    Update Debian Build environment

--- projects/debian-build-env-jdk8/config.sh
+++ projects/debian-build-env-jdk8/config.sh
@@ -7,10 +7,10 @@ CONTAINER_PROJECT="build-env"
 
 # Base Image Dependency
 BASE_IMAGE="opennms/openjdk"
-BASE_IMAGE_VERSION="debian-jdk8-b5054"
+BASE_IMAGE_VERSION="debian-jdk8-b5818"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 SHELLCHECK_VERSION="0.6.0"
-DOCKER_COMPOSE_VERSION="1.25.5"
+DOCKER_COMPOSE_VERSION="1.27.4"
 
 # inherit $BASE_IMAGE_VERSION
 PARENT_PROJECT="debian-openjdk-8"
