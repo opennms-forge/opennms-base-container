7117875ca3794860952cab05f5afaafbf19de0bf
Ronny Trommer <ronny@opennms.org>
2020-12-23 07:55:19 +0100

    Update Debian OpenJDK 11 and OpenJDK 1.8

--- projects/debian-openjdk-11/config.sh
+++ projects/debian-openjdk-11/config.sh
@@ -7,20 +7,16 @@ CONTAINER_PROJECT="openjdk"
 
 # Base Image Dependency
 BASE_IMAGE="opennms/debian"
-BASE_IMAGE_VERSION="stretch-b4955"
+BASE_IMAGE_VERSION="stretch-b5775"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 SHORT_DATE="$(date -u +"%Y%m%d%H%M%S")"
 
 # inherit $BASE_IMAGE_VERSION
 PARENT_PROJECT="debian"
 
-#VM_TYPE="openj9"
-
-#IMAGE_VERSION=("debian-jdk11-${VM_TYPE}")
 IMAGE_VERSION=("debian-jdk11")
 
 # Most specific tag when it is not build locally and in CircleCI
 if [ -n "${CIRCLE_BUILD_NUM}" ]; then
-  #IMAGE_VERSION+=("debian-jdk11-${VM_TYPE}-b${CIRCLE_BUILD_NUM}")
   IMAGE_VERSION+=("debian-jdk11-b${CIRCLE_BUILD_NUM}")
 fi
