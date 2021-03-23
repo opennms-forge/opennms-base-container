71607b6714d9b937a60adc041c20d128e501fcce
Ronny Trommer <ronny@opennms.org>
2021-03-23 09:34:13 +0100

    Add Antora image with plugins, git and openssh client

---
+++ projects/antora/build_container_image.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+set -x
+
+# shellcheck source=projects/confd/config.sh
+source ./config.sh
+
+# shellcheck source=projects/registry-config.sh
+source ../registry-config.sh
+
+docker build -t "${CONTAINER_PROJECT}:${IMAGE_VERSION[0]}" \
+  --build-arg BASE_IMAGE="${BASE_IMAGE}" \
+  --build-arg BASE_IMAGE_VERSION="${BASE_IMAGE_VERSION}" \
+  --build-arg BUILD_DATE="${BUILD_DATE}" \
+  --build-arg SOURCE="${CIRCLE_REPOSITORY_URL}" \
+  --build-arg REVISION="$(git describe --always)" \
+  --build-arg BUILD_JOB_ID="${CIRCLE_WORKFLOW_JOB_ID}" \
+  --build-arg BUILD_NUMBER="${CIRCLE_BUILD_NUM}" \
+  --build-arg BUILD_URL="${CIRCLE_BUILD_URL}" \
+  --build-arg BUILD_BRANCH="${CIRCLE_BRANCH}" \
+  --build-arg VERSION="${VERSION}" \
+  .
+
+docker image save "${CONTAINER_PROJECT}:${IMAGE_VERSION[0]}" -o "${CONTAINER_IMAGE}"
+
---
+++ projects/antora/config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+# shellcheck disable=SC2034
+
+# Configure base image dependency
+BASE_IMAGE="antora/antora"
+BASE_IMAGE_VERSION="2.3.4"
+VERSION=${BASE_IMAGE_VERSION}
+IMAGE_VERSION=("${VERSION}")
+
+BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
+
+# Most specific tag when it is not build locally and in CircleCI
+if [ -n "${CIRCLE_BUILD_NUM}" ]; then
+  IMAGE_VERSION+=("${VERSION}-b${CIRCLE_BUILD_NUM}")
+fi
