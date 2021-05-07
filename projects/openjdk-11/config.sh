09e4134e2d2f29f088f4fe40c01e8f044d3956a3
Stefan Wachter <stefan.wachter@gmx.de>
2021-05-07 16:46:03 +0200

    adapt centos image version

--- projects/openjdk-11/config.sh
+++ projects/openjdk-11/config.sh
@@ -7,7 +7,7 @@ CONTAINER_PROJECT="openjdk"
 
 # Base Image Dependency
 BASE_IMAGE="opennmsdcttest/centos"
-BASE_IMAGE_VERSION="8-b5649"
+BASE_IMAGE_VERSION="8-b6457"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 
 # inherit $BASE_IMAGE_VERSION
