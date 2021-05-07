0c57e154a81fbfe2ef460a7f0936bb8bc99cc719
Stefan Wachter <stefan.wachter@gmx.de>
2021-05-07 15:43:22 +0200

    switch to opennmsdcttest repo; set build context for maven-jdk* jobs

--- projects/registry-config.sh
+++ projects/registry-config.sh
@@ -11,7 +11,7 @@ fi
 
 # Container registry and tags
 CONTAINER_REGISTRY="docker.io"
-CONTAINER_REGISTRY_REPO="opennms"
+CONTAINER_REGISTRY_REPO="opennmsdcttest"
 CONTAINER_VERSION_TAGS=${IMAGE_VERSION[*]}
 
 # Container Image Artifact
