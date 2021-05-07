dd08d32b2e329f3cbcca50629d939b60269ded21
Stefan Wachter <stefan.wachter@gmx.de>
2021-05-07 17:03:45 +0200

    tag centos image for use as base image

--- projects/centos/build_container_image.sh
+++ projects/centos/build_container_image.sh
@@ -22,3 +22,5 @@ docker build -t "${CONTAINER_PROJECT}:${IMAGE_VERSION[0]}" \
   .
 
 docker image save "${CONTAINER_PROJECT}:${IMAGE_VERSION[0]}" -o "${CONTAINER_IMAGE}"
+
+docker tag "${CONTAINER_PROJECT}:${IMAGE_VERSION[0]}" "opennmsdcttest/${CONTAINER_PROJECT}:${IMAGE_VERSION[0]}-b${CIRCLE_BUILD_NUM}"
