48952477a2779e7d62e41efa32e259ac93ed8464
Stefan Wachter <stefan.wachter@gmx.de>
2021-05-11 14:38:25 +0200

    disable DCT for docker build

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -25,7 +25,7 @@ env | grep DOCKER
 
 #echo "Detect changes in projects"
 # if found_changes; then
-  export DOCKER_CONTENT_TRUST=0 && ./build_container_image.sh && export DOCKER_CONTENT_TRUST=1 \
+  ./build_container_image.sh \
   ~/opennms-container/.circleci/tag.sh && \
   ~/opennms-container/.circleci/publish.sh
 #else
