1eac89ec3380e335f6e9beb47516cfeb38e8fb48
Benjamin Reed <ranger@opennms.org>
2022-09-21 12:53:53 -0400

    add a few more dependencies used in circle builds

--- projects/circleci-ubuntu-build-env-jdk8/config.sh
+++ projects/circleci-ubuntu-build-env-jdk8/config.sh
@@ -28,4 +28,4 @@ REPO_RELEASE="stable"
 REPO_HOST="debian.opennms.org"
 REPO_KEY_URL="https://${REPO_HOST}/OPENNMS-GPG-KEY"
 
-PACKAGES="nodejs autoconf automake build-essential ca-certificates cmake curl debhelper devscripts dh-systemd docker-ce dpkg-sig expect git librrd-dev libtool make nsis po-debconf python python3 rsync shellcheck ssh thin-provisioning-tools"
+PACKAGES="nodejs autoconf automake build-essential ca-certificates cmake curl debconf-utils debhelper devscripts dh-systemd docker-ce dpkg-sig expect git librrd-dev libtool make nsis po-debconf python python3 rsync shellcheck software-properties-common ssh thin-provisioning-tools"
