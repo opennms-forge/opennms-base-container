5b5a14530be9c266ba3de9f599ada1c1ecd33182
Benjamin Reed <ranger@opennms.org>
2023-06-01 15:07:44 -0400

    bump debian-based stuff to buster

--- projects/gulp/config.sh
+++ projects/gulp/config.sh
@@ -4,7 +4,7 @@
 
 # Base Image Dependency
 BASE_IMAGE="node"
-BASE_IMAGE_VERSION="11-stretch"
+BASE_IMAGE_VERSION="16-buster"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 
 # Version information
